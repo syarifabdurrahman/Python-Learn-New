442015cb5f371f476808b6df753b689a6e9eb189
unknown <syarifabdr22@gmail.com>
2022-10-03 09:46:30 +0700

    Memahami percabangan

---
+++ fundamental.py
@@ -0,0 +1,19 @@
+# Sekuinsial
+# print('Ibu berkata, "Pergi ke toko"')
+# print('Budi menjawab, "Oke, apa yang harus dilakukan disana?"')
+# print('Ibu berkata,"Belilah satu botol susu, jika ada telur, belilah 1 botol susu dan 6 telur"')
+# print("Maka budi berangkat")
+# print("Budi mulai berbelanja")
+
+# Percabangan
+bottle_of_milk = 100
+has_egg=True
+
+if bottle_of_milk > 0:
+    print ("Budi mengecek uangnya cukup atau tidak, ternya cukup")
+    if has_egg:
+        print("Budi membeli 1 botol susu dan 6 telur")
+    else:
+        print("Budi membeli satu botol susu")
+else:
+    print("Budi tidak membeli 1 botol susu")
